0aa255b371e70eaadcb388c6f5f3205e0d37c0ad
Ashu Singh <singh.ashu04072001@gmail.com>
2024-01-06 23:48:24 +0530

    Committing to largest number
    commit to largest number using if else

---
+++ JavascriptConcept/Condition_program/if_elseif/Largest_num.js
@@ -0,0 +1,12 @@
+let a1 = 600 ;
+let b1 = 500 ;
+let c1 = 1000 ;
+if(a1 > b1 && a1 > c1){
+    console.log(a1);
+}
+else if(b1 > a1 && b1 > c1){
+    console.log(b1);
+}
+else {
+    console.log(c1);
+}
