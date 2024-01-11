c8bb451409325596e43532f4500274ea5581930c
Ashu Singh <singh.ashu04072001@gmail.com>
2024-01-11 23:19:55 +0530

    committing to concept hosting
    commit to concept hosting

---
+++ JavascriptConcept/Hosting_concept/hosting.js
@@ -0,0 +1,14 @@
+console.log(a)
+var a = 30;
+// console.log(a)
+a1()
+function a1(){
+    console.log("Ashu is here")
+}
+// a1()
+var b;
+console.log(b);
+// console.log(d);
+// const d = 25;
+console.log(e);
+let e = 5;
