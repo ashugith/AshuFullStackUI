5f8990c6c76d1320a5ad85aa7519759580ca9c22
Ashu Singh <singh.ashu04072001@gmail.com>
2024-01-15 23:51:12 +0530

    committing to reverse program
    commit to reverse program

---
+++ JavascriptConcept/Reverse_prog/Reverse.js
@@ -0,0 +1,12 @@
+function reverseFunction (num){
+    let numStr = num.toString();
+    let reversedStr = '';
+    for(let i = numStr.length-1 ; i>=0 ; i--)
+    {
+        reversedStr += numStr[i];
+    }
+    return parseInt(reversedStr);
+}
+let num = 6758497 ;
+let reversedNum = reverseFunction(num);
+document.write(reversedNum)
